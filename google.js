ba0c7cf16186ab0a9d512d3d1a307aea7ecfcdca
sarathbabu <parasarathbabu5@gmail.com>
2024-09-27 16:58:30 +0530

    update

---
+++ google.js
@@ -0,0 +1,64 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_BZ9Y3dWykEDSNdo_CGiKaenJMGzUMPo",
+  authDomain: "instagram-clone55.firebaseapp.com",
+  databaseURL: "https://instagram-clone55-default-rtdb.firebaseio.com",
+  projectId: "instagram-clone55",
+  storageBucket: "instagram-clone55.appspot.com",
+  messagingSenderId: "740432944595",
+  appId: "1:740432944595:web:9636871180603608792326",
+};
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+
+const auth = getAuth(app);
+
+document.getElementById("button").addEventListener("click", function () {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      window.location.href = "main.html";
+      console.log(user);
+
+      //   document.getElementById("logout").style.display = "block";
+
+      //   document.getElementById("logout").innerText = `logout `;
+
+      //   alert("registered");
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+});
+
+document.getElementById("logout").addEventListener("click", function () {
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      console.log("sign-out successful");
+      alert("sign- out successful");
+      document.getElementById("logout").style.display = "none";
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
--- login.js
+++ login.js
@@ -1,15 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import {
   getAuth,
-  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {
   getDatabase,
   ref,
-  set,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 
 const firebaseConfig = {
@@ -22,32 +21,34 @@ const firebaseConfig = {
   appId: "1:740432944595:web:9636871180603608792326",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-document.getElementById("register").addEventListener("click", function () {
+
+const auth = getAuth(app);
+
+const db = getDatabase();
+
+document.getElementById("login").addEventListener("click", function () {
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
-  let name = document.getElementById("name").value;
-  const auth = getAuth(app);
-  createUserWithEmailAndPassword(auth, email, password, name)
+
+  signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed up
+      // Signed in
       const user = userCredential.user;
 
-      const db = getDatabase();
-      set(ref(db, "users/" + user.uid), {
-        password: password,
-        email: email,
-        name: name,
+      const user_ref = ref(db, "users/" + user.uid);
+      onValue(user_ref, (snapshot) => {
+        const data = snapshot.val();
+        console.log("data", data);
       });
-      window.location.reload();
+
+      console.log("user", user);
+      alert("login successfully");
     })
-    // ...
 
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      alert(errorCode, errorMessage);
-      console.log(errorCode);
+      alert(errorMessage);
     });
 });
